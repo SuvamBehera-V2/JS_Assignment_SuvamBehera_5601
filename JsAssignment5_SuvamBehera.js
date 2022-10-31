5de69c22d645a15326471f7d203ba71598d3051a
Suvam Behera <suvam.behera@v2solutions.com>
2022-10-31 15:46:14 +0530

    Corrections to the logic

--- JsAssignment5_SuvamBehera.js
+++ JsAssignment5_SuvamBehera.js
@@ -3,9 +3,16 @@ let userInput = prompt("Enter the text you want to transform: ");
 function charReplace(text){
     let allChar = text.split('')
     for (let i = 0; i<allChar.length; i++){
-        allChar.splice(i, 1, String.fromCharCode(allChar[i].charCodeAt(0) + 1))
+        switch(allChar[i]){
+            case ' ':
+                break;
+            case 'z':
+                allChar[i] = 'a';
+            default:
+                allChar.splice(i, 1, String.fromCharCode(allChar[i].charCodeAt(0) + 1))
+        }
     }
-    return allChar.join('')
+    return allChar.join('');
 }
 
 console.log(charReplace(userInput));
